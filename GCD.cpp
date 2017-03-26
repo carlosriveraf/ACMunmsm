fbffc467d3cb8a7a97415b8ac1a6e964e08b24fa
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:13:14 -0500

    Update and rename GCD to GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int GCD(int a, int b){
-     if(b==0) return a;
-     return GCD (b, a%b);
+int GCD( int a, int b ) {
+     if( b == 0 ) return a;
+     return GCD ( b, a % b );
 }
 
 int main(){
